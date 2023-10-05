89c2dd55dcf313d5dc8a06612d046b853862395a
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-10-05 12:58:01 -0700

    Hello,puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main- the main function
+ *
+ * Return-is 0 in this case 
+ */
+ 
+ 
+int main(void)
+{        puts("\"Programming is like building a multilingual puzzle");
+         return (0);
+
+}	 
