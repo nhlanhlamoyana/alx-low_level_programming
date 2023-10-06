d139e8c14cd5623c3f97da1ba3741641d53e4e13
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-10-06 15:59:07 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -8,12 +8,16 @@
 
 int main(void)
 {
-	int i;
+        int i;
 	
-	for (i = 0 ; i < 10 ; i++)
-	{      putchar(i);
-	       putchar(',');
-	       putchar(' ');
+        for (i = 0 ; i < 10 ; i++)
+	{      
+                putchar(i + '0');
+                if (i ! = 9)
+		{
+			putchar('.');
+			putchar(' ')
+                }
 	}
 	putchar('\n');
 	return (0);
