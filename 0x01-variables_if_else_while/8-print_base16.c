411c2017636897b5886e2c2f9eb1e6575431459a
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-10-06 17:41:51 -0700

    Hexadecimal

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -5,14 +5,16 @@
  *
  * Return: always 0
  */
+
 int main(void)
 {
-	int  i;
+	int i;
 	char j;
 
-	for  (i = 0 ; i < 10 ; i++)
-		putchar (i + '0');
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
 	for (j = 'a' ; j <= 'f' ; j++)
 		putchar(j);
 	putchar('\n');
-}       return(0);
+	return (0);
+}
