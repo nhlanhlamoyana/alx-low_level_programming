9996ed9db3b69c42bd322814a32c56a1fbfa4c92
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-10-10 17:43:36 -0700

    Sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - a function that prints the sign of a number
+ * @n: The number
+ * Return: 1 for all
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+	      _putchar(43);
+	      return (1);
+
+	 }
+	
+	else if (n < 0)
+        {
+		 _putchar(45);
+		 return (-1); 
+         }
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+
+}
