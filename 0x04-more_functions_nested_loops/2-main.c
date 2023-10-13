c6e3039db74f75c21de2374122f68a156f81a325
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-10-12 17:09:43 -0700

    task 2

--- 0x04-more_functions_nested_loops/2-main.c
+++ 0x04-more_functions_nested_loops/2-main.c
@@ -8,7 +8,8 @@
  */
 int main(void)
 {
-    printf("%d\n", mul(98, 1024));
-    printf("%d\n", mul(-402, 4096));
-    return (0);
+int mul(a. int b)
+printf("%d\n", mul(98, 1024));
+printf("%d\n", mul(-402, 4096));
+return (0);
 }
