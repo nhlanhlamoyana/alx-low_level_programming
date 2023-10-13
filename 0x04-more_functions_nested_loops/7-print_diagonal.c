8f6865bb43f08012a4c029a4eb1c85b604653de9
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-10-12 20:12:46 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a disgonal line on the terminal
+ * @n: n is number of times the character '\' should be printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		int i, j;
+
+		for (i = 0 ; i < n ; i++)
+		{
+			for (j = 0 ; j <= i ; j++)
+				_putchar(' ');
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+
+
+
+}
