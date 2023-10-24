3c3e7a471208e74f196e8492669bd58648ef637a
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2023-10-23 18:21:29 -0700

    strpbrk

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strpbrk - a function that searchesa string for any of a set of bytes
+ * @s: string to find from
+ * @accept: string to find in s
+ * Return: NULL or accept
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+while (*s != '\0')
+{
+const char *a = accept;
+
+while (*a != '\0')
+{
+if (*s == *a)
+return (s);
+a++;
+}
+s++;
+}
+
+return (NULL);
+}
