d2481f5fb584672a3c4c40e363ccdd3d2938be04
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-02-04 05:47:52 -0800

    a function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -10,43 +10,32 @@
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	hash_node_t *new;
-	char *value_copy;	
+	unsigned long int index;
+	hash_node_t *hash_node, *tmp;
+	char *new_value;
 
-	unsigned long int index i;
-
-	if (ht == NULL || key == NULL || *key == '\0' ||
-value == NULL)
-		return (0);
-	value_copy = strdup(value);
-	if (value_copy == NULL)
+	if (ht == NULL || ht->array == NULL || ht->size == 0 ||
+	    key == NULL || strlen(key) == 0 || value == NULL)
 		return (0);
 	index = key_index((const unsigned char *)key, ht->size);
-	for (i = index; ht->array[i]; i++)
+	tmp = ht->array[index];
+	while (tmp != NULL)
 	{
-		if (strcmp(ht->array[i]->key, key) == 0)
+		if (strcmp(tmp->key, key) == 0)
 		{
-			free(ht->array[i]->value);
-			ht->array[i]->value = value_copy;
+			new_value = strdup(value);
+			if (new_value == NULL)
+				return (0);
+			free(tmp->value);
+			tmp->value = new_value;
 			return (1);
 		}
+		tmp = tmp->next;
 	}
-	new = malloc(sizeof(hash_node_t));
-	if (new == NULL)
-	{
-
-		free(value_copy);
+	hash_node = make_hash_node(key, value);
+	if (hash_node == NULL)
 		return (0);
-	}
-	new->key = strdup(key);
-	if (new->key == NULL)
-	{
-		free(new);
-		return (0);
-	}
-	new->value = value_copy;
-	new->next = ht-
-		>array[index];
-	ht->array[index] = new;
-	return (1);	
-}
+	hash_node->next = ht->array[index];
+	ht->array[index] = hash_node;
+	return (1);
+		
