2e87c30771c80604ce43b326cb2f292555e4712e
nhlanhlamoyana <moyananhlanhla27@gmail.com>
2024-02-04 06:28:24 -0800

    a function that adds an element to the hash table

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,6 +1,38 @@
 #include "hash_tables.h"
 
 /**
+ * make_hash_node - creates a new hash node
+ * @key: key for the node
+ * @value: for the node
+ *
+ * Return: the new node, or Null on failure
+ */
+hash_node_t *make_hash_node(const char *key, const char *value)
+{
+	hash_node_t *node;
+
+	node = malloc(sizeof(hash_node_t));
+	if (node == NULL)
+		return (NULL);
+	node->key = strdup(key);
+	if (node->key == NULL)
+	{
+		free(node);
+		return (NULL);
+	}
+	node->value = strdup(value);
+	if (node->value == NULL)
+	{
+		free(node->key);
+		free(node);
+		return (NULL);
+	}
+	node->next = NULL;
+	return (node);
+}
+
+
+/**
  * hash_table_set - Add or update an element in a hash table.
  * @ht: A pointer to the hash table.
  * @key: The key to add - cannot be an emoty string.
@@ -38,4 +70,4 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node->next = ht->array[index];
 	ht->array[index] = hash_node;
 	return (1);
-		
+}
